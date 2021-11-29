db7704e2f33fd9a451c9d88940e3c53bcf1892db
mvs.meah <mvs.meah@yahoo.com>
2021-11-29 15:13:28 +0800

    added sign in button

--- app/src/main/java/com/example/pakigsabot/Signup.java
+++ app/src/main/java/com/example/pakigsabot/Signup.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Signup extends AppCompatActivity {
 
     ImageButton prev;
+    Button signin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +27,27 @@ public class Signup extends AppCompatActivity {
             }
         });
 
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signInScreen();
+            }
+        });
+
     }
 
     public void refs(){
         prev = findViewById(R.id.backBtnSU);
+        signin = findViewById(R.id.signinBtnSUS);
     }
 
     public void welcomeScreen(){
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void signInScreen(){
+        Intent intent = new Intent(getApplicationContext(), Signin.class);
         startActivity(intent);
     }
 
