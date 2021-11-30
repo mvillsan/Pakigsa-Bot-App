be79a029e7df15c8cc8594d38922c38715f200a5
mvs.meah <mvs.meah@yahoo.com>
2021-11-30 17:04:30 +0800

    Changes layout design signin and signup

--- app/src/main/java/com/example/pakigsabot/Signin.java
+++ app/src/main/java/com/example/pakigsabot/Signin.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class Signin extends AppCompatActivity {
 
-    ImageButton prev;
-    Button signup;
+    ImageView show_password,prev;
+    EditText password;
+    TextView signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +38,20 @@ public class Signin extends AppCompatActivity {
                 signUpScreen();
             }
         });
+
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowHidePass(view);
+            }
+        });
     }
 
     public void refs(){
         prev = findViewById(R.id.backBtnSignIn);
-        signup = findViewById(R.id.signupBtnSIS);
+        signup = findViewById(R.id.signUpTxtView);
+        show_password = findViewById(R.id.show_passImageView);
+        password = findViewById(R.id.passwordEditTxtSI);
     }
 
     public void welcomeScreen(){
@@ -49,5 +62,24 @@ public class Signin extends AppCompatActivity {
     public void signUpScreen(){
         Intent intent = new Intent(getApplicationContext(), Signup.class);
         startActivity(intent);
+    }
+
+    public void ShowHidePass(View view){
+
+        if(view.getId()==R.id.show_passImageView){
+
+            if(password.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())){
+                ((ImageView)(view)).setImageResource(R.drawable.hide_passwordicon);
+
+                //Show Password
+                password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            }
+            else{
+                ((ImageView)(view)).setImageResource(R.drawable.show_passwordicon);
+
+                //Hide Password
+                password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        }
     }
 }
