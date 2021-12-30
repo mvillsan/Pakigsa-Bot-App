cf8ad2d1855c792f552c64976d5c250398d5b2df
mvs.meah <mvs.meah@yahoo.com>
2021-12-30 19:29:38 +0800

    added resort details specific layout

--- app/src/main/java/com/example/pakigsabot/Resorts/ResortSpecificDetails.java
+++ app/src/main/java/com/example/pakigsabot/Resorts/ResortSpecificDetails.java
@@ -2,17 +2,14 @@ package com.example.pakigsabot.Resorts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
 import com.example.pakigsabot.R;
 
-public class CheckInTimeReserve2 extends AppCompatActivity {
+public class ResortSpecificDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_in_time_reserve2);
+        setContentView(R.layout.activity_resort_specific_details);
     }
 }
