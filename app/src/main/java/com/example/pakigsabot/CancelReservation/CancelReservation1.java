f22ec1abf3b882ed864cc354d820c6d825fc0578
mvs.meah <mvs.meah@yahoo.com>
2022-01-06 18:58:05 +0800

    added clear close icon

---
+++ app/src/main/java/com/example/pakigsabot/CancelReservation/CancelReservation1.java
@@ -0,0 +1,15 @@
+package com.example.pakigsabot.CancelReservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.pakigsabot.R;
+
+public class CancelReservation1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancel_reservation1);
+    }
+}
