4c1a6106e595073534747d95480355577bd4edf5
mvs.meah <mvs.meah@yahoo.com>
2022-01-08 23:27:53 +0800

    added chatbot conversation layout

--- app/src/main/java/com/example/pakigsabot/Chatbot/ChatbotConversation5.java
+++ app/src/main/java/com/example/pakigsabot/Chatbot/ChatbotConversation5.java
@@ -1,38 +1,38 @@
 package com.example.pakigsabot.Chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.pakigsabot.R;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.pakigsabot.R;
-public class ChatbotConversation3 extends AppCompatActivity {
+public class ChatbotConversation5 extends AppCompatActivity {
 
-    TextView homeMenuTxt2CC3;
+    TextView homeMenuTxt2CC5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chatbot_conversation3);
+        setContentView(R.layout.activity_chatbot_conversation5);
 
         refs();
 
-        homeMenuTxt2CC3.setOnClickListener(new View.OnClickListener() {
+        homeMenuTxt2CC5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chatbotConversation4();
+                chatbotConversation6();
             }
         });
     }
 
     public void refs(){
-        homeMenuTxt2CC3 = findViewById(R.id.homeMenuTxt2CC3);
+        homeMenuTxt2CC5 = findViewById(R.id.homeMenuTxt2CC5);
     }
 
-    private void chatbotConversation4(){
-        Intent intent = new Intent(getApplicationContext(), ChatbotConversation4.class);
+    private void chatbotConversation6(){
+        Intent intent = new Intent(getApplicationContext(), ChatbotConversation6.class);
         startActivity(intent);
     }
 }
