e8b129b8e678c1103e29730c85ae3e8fef84101f
mvs.meah <mvs.meah@yahoo.com>
2022-01-10 00:15:16 +0800

    added compose email to the establishment layout

---
+++ app/src/main/java/com/example/pakigsabot/EmailMessage/EmailMessageLayout.java
@@ -0,0 +1,15 @@
+package com.example.pakigsabot.EmailMessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.pakigsabot.R;
+
+public class EmailMessageLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_message_layout);
+    }
+}
--- app/src/main/java/com/example/pakigsabot/Resorts/ResortReservationSuccess.java
+++ app/src/main/java/com/example/pakigsabot/Resorts/ResortReservationSuccess.java
@@ -1,14 +1,40 @@
 package com.example.pakigsabot.Resorts;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.pakigsabot.EmailMessage.EmailMessageLayout;
 import com.example.pakigsabot.R;
 
 public class ResortReservationSuccess extends AppCompatActivity {
+
+    TextView emailTxtResort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resort_reservation_success);
+
+        refs();
+
+        emailTxtResort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                emailMessageScreen();
+            }
+        });
+    }
+
+    public void refs(){
+        emailTxtResort = findViewById(R.id.emailTxtResort);
+    }
+
+    private void emailMessageScreen(){
+        Intent intent = new Intent(getApplicationContext(), EmailMessageLayout.class);
+        startActivity(intent);
     }
 }
