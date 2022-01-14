22d60dc5edde9247b1eedda4df7466321f731f06
mvs.meah <mvs.meah@yahoo.com>
2022-01-14 23:41:49 +0800

    added default convo chatbot

--- app/src/main/java/com/example/pakigsabot/Chatbot/ChatbotConversation6.java
+++ app/src/main/java/com/example/pakigsabot/Chatbot/ChatbotConversation6.java
@@ -2,14 +2,39 @@ package com.example.pakigsabot.Chatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.example.pakigsabot.R;
 
 public class ChatbotConversation6 extends AppCompatActivity {
+
+    TextView homeMenuTxt2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot_conversation6);
+
+        //References:
+        refs();
+
+        homeMenuTxt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                defaultConvo();
+            }
+        });
+    }
+
+    public void refs(){
+        homeMenuTxt2 = findViewById(R.id.homeMenuTxt2);
+    }
+
+    private void defaultConvo(){
+        Intent intent = new Intent(getApplicationContext(), ChatbotConversationDefault.class);
+        startActivity(intent);
     }
 }
